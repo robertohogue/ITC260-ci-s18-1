e1cf8cd17b72045fdc338685a647627fb2940c2d
robertohogue <36170635+robertohogue@users.noreply.github.com>
2018-08-04 14:12:30 -0700

    Config folder for Pics function
    Adding Flickr to the app

---
+++ application/config/custom_config.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//custom_config.php - a place to save global settings
+
+$config['style'] = 'yeti.css';
+$config['banner'] = 'Default Banner';
+$config['title'] = 'Default Title';
+$config['copyright'] = 'Default Copyright';
+$config['masthead'] = 'Default MastHead';
+$config['theme'] = 'themes/bootswatch/';
+$config['flickr_api_key']= 'd12cdb7342e3c9b514b8e54c0ad4b50f';
